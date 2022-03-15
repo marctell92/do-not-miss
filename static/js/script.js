8375eeb64fd02c3dd038157e7756718836adfc2b
marctell92 <marctell92@gmail.com>
2022-03-15 19:13:06 +0000

    Add festival form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 5,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
