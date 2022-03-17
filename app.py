9bec6ec185fdfc770509c72506c5a3fd8eb5ab60
marctell92 <marctell92@gmail.com>
2022-03-17 08:15:19 +0000

    Add Search functionality

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_festivals():
     return render_template("festivals.html", festivals=festivals)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    festivals = list(mongo.db.festivals.find({"$text": {"$search": query}}))
+    return render_template("festivals.html", festivals=festivals)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
